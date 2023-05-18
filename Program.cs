a32b072a342d83f31dac13968ec10dd9c12ed99b
nikitailinskii <anastasiabalickaa435@gmail.com>
2023-05-18 12:47:56 +0300

    экзамен

--- Program.cs
+++ Program.cs
@@ -10,6 +10,46 @@ namespace ConsoleAppдаа
     {
         static void Main(string[] args)
         {
+            /* 1задиние*/
+            //Console.Write("x=");
+            //int x = int.Parse(Console.ReadLine());
+            //Console.Write("y=");
+            //int y = int.Parse(Console.ReadLine());
+            //Console.WriteLine($"{x}+{y}={x + y}");
+            //Console.ReadLine();
+
+            //2 задание 2
+            //Console.Write("x=");
+            // int x = int.Parse(Console.ReadLine());
+            // Console.Write("y=");
+            // int y = int.Parse(Console.ReadLine());
+            // Console.Write("z=");
+            // int z = int.Parse(Console.ReadLine());
+            // Console.WriteLine($"{z}+{x}+{y}={z+ x + y}");
+            // Console.ReadLine();
+            //3задание 3
+            //    Console.Write("x=");
+            //double x = double.Parse(Console.ReadLine());
+            //Console.Write("y=");
+            //double y = double.Parse(Console.ReadLine());
+            //Console.WriteLine($"{x:F1}*{y:F1}={x * y:F1}");
+            //Console.ReadLine();
+            //задани 4
+            //Console.Write("x=");
+            //double x = double.Parse(Console.ReadLine());
+            //Console.Write("y=");
+            //double y = double.Parse(Console.ReadLine());
+            //Console.WriteLine($"{x:F3}/{y:F3}={x / y:F3}");
+            //Console.ReadLine();
+            //Задание 9
+            //Console.Write("a=");
+            //int a = int.Parse(Console.ReadLine());
+            //Console.Write("y=");
+            //int b = int.Parse(Console.ReadLine());
+            //Console.WriteLine($"P={2 * (a + b)}");
+
+
+
         }
     }
 }
